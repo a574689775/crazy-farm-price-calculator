2ea263501598c9d9d07805dc9e161ace8799e6b0
chenzhaosheng <chenzhaosheng@baidu.com>
2026-03-05 10:37:11 +0800

    v7.8.2

--- src/data/changelog.ts
+++ src/data/changelog.ts
@@ -6,6 +6,13 @@ export interface ChangelogItem {
 
 export const changelog: ChangelogItem[] = [
   {
+    version: 'V7.8.2',
+    date: '2026.3.5',
+    items: [
+      '更新月影梅月球作物重量上限',
+    ],
+  },
+  {
     version: 'V7.8.1',
     date: '2026.3.5',
     items: [
